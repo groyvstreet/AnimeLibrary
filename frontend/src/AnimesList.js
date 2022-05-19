9d476f2e777b17bc1c262ded2aa0d87141ebfa74
Kraken <kirya.polyakov.2003@mail.ru>
2022-05-20 02:19:25 +0300

    frontend updated: animes list added, sort added

--- frontend/src/AnimesList.js
+++ frontend/src/AnimesList.js
@@ -1,6 +1,21 @@
-function AnimesList() {
+import axios from "axios";
+import {useState} from "react";
+import AnimeItem from "./components/AnimeItem";
+
+function AnimesList({animes}) {
+    async function animesList() {
+        const response = await axios.get('http://127.0.0.1:8000/api/animes/')
+        console.log(response.data)
+    }
+
     return (
-        <div>Список</div>
+        <div>
+            <button onClick={animesList}>GET</button>
+            <h1>Аниме</h1>
+            {animes.map((anime) =>
+                <AnimeItem anime={anime} key={anime.id}/>
+            )}
+        </div>
     );
 }
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,49 @@
 import './App.css';
 import Layout from './Layout'
 import AnimesList from "./AnimesList";
+import {useState} from "react";
+import SortSelect from "./components/SortSelect";
 
 function App() {
+    const [animes, setAnimes] = useState([
+        {id: 1, title: 'Naruto', date: 'b', rating: 'a'},
+        {id: 2, title: 'Boruto', date: 'a', rating: 'b'},
+    ])
+
+    const [selectedSort, setSelectedSort] = useState('')
+
+    const sortAnimes = (sort) => {
+        setSelectedSort(sort);
+        const newAnime = {
+            id: Date.now(),
+            title: 'Test',
+            date: 'Test',
+            rating: 'Test',
+        }
+        setAnimes([...animes].sort((a, b) => a[sort].localeCompare(b[sort])))
+    }
+
     return (
         <div className="App">
-            <Layout />
-            <AnimesList />
+            <Layout/>
+            <div>
+                <SortSelect
+                    value={selectedSort}
+                    onChanged={sortAnimes}
+                    options={[
+                        {value: 'rating', name: 'По рейтингу'},
+                        {value: 'date', name: 'По году'},
+                    ]}
+                />
+            </div>
+            {animes.length !== 0
+                ?
+                <AnimesList animes={animes}/>
+                :
+                <h1>
+                    Список пуст
+                </h1>
+            }
         </div>
     );
 }
