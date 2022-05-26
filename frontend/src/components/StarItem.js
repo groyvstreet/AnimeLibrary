a96a8cdf58333497c4e623a4515f2e450b894331
Kraken <kirya.polyakov.2003@mail.ru>
2022-05-26 23:15:55 +0300

    now is possible to create, update anime rating

---
+++ frontend/src/components/StarItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Star = () => {
+    return (
+        <svg width="32" height="32" viewBox="0 0 32 32">
+            <use href="#star" fill="gray"></use>
+        </svg>
+    )
+}
+
+export default Star
